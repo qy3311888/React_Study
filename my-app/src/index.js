5cdbbdffcbf831aa566441bee5545f8b44e359bf
qy3311888 <qy3311888@163.com>
2023-06-03 19:25:09 +0800

    React组件

---
+++ my-app/src/index.js
@@ -0,0 +1,9 @@
+import React from "react"; // React17之后，不需要再进行引入
+import ReactDOM from "react-dom";
+import App from "./01-base/01-class_component";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+/**
+ * jsx == js + xml
+ */
