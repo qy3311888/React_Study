2b5d4b22dbd975005b50e494f56aba7f340d061b
qy3311888 <qy3311888@163.com>
2023-06-17 11:04:00 +0800

    Component Study

--- my-app/src/01-base/01-class_component.js
+++ my-app/src/01-base/01-class_component.js
@@ -2,7 +2,14 @@ import React from "react";
 
 class App extends React.Component {
   render() {
-    return <div>hello react Component</div>;
+    return (
+      <div>
+        hello react Component
+        <ul>
+          <li>11</li>
+        </ul>
+      </div>
+    );
   }
 }
 
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,8 +1,8 @@
-import React from "react"; // React17之后，不需要再进行引入
-import ReactDOM from "react-dom";
-import App from "./01-base/01-class_component";
+import React from 'react' // React17之后，不需要再进行引入
+import ReactDOM from 'react-dom'
+import App from './01-base/03-nested_component'
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'))
 
 /**
  * jsx == js + xml
