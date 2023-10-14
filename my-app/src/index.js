f2d8c055b1b5b22e74ef547b4370bf8dbea78fe6
qy3311888 <qy3311888@163.com>
2023-10-14 22:53:54 +0800

    Clear

--- my-app/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react' // React17之后，不需要再进行引入
-import ReactDOM from 'react-dom'
-import App from './01-base/03-nested_component'
-
-ReactDOM.render(<App />, document.getElementById('root'))
-
-/**
- * jsx == js + xml
- */
